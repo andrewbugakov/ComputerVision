98fa87fcd29691e54e30427db5385016a4a42149
Andrei Bugakov <andrei_bugakov@epam.com>
2015-11-05 20:51:11 +0400

    reComVis

---
+++ ComputerVision/src/main/java/Main.java
@@ -0,0 +1,39 @@
+import UI.MainShell;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class Main {
+
+    private static void runmainshell(){
+        Display display = new Display ();
+        MainShell mainShell=new MainShell(display);
+        display.dispose ();
+    }
+    public static void main (String [] args) {
+        runmainshell();
+    }
+    private static void show(){
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("Label test");
+        new Label(shell, SWT.SEPARATOR | SWT.HORIZONTAL);
+        Label lblHello = new Label(shell, SWT.NONE);
+        lblHello.setText("Simple Label");
+        new Label(shell, SWT.SEPARATOR | SWT.HORIZONTAL);
+        shell.setLayout(new GridLayout());
+//        Label lblDuke = new Label(shell, SWT.NONE);
+//        lblDuke.setImage(new Image(Display.getCurrent(), getClass()
+//                .getResourceAsStream("/by/bs/swt/images/duke_waving.gif")));
+        shell.pack();
+        shell.open();
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch())
+                display.sleep();
+        }
+        shell.dispose();
+        display.dispose();
+    }
+}
---
+++ ComputerVision/src/main/java/UI/ShellSelection.java
@@ -0,0 +1,8 @@
+package UI;
+
+/**
+ * Created by Andrei_Bugakov on 11/5/2015.
+ */
+public class ShellSelection {
+
+}
