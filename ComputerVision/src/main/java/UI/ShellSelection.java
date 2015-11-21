5699c0b8623970717af96d0562acfe1df0a96dcb
Andrei Bugakov <andrei_bugakov@epam.com>
2015-11-21 19:47:58 +0400

    good commit

--- ComputerVision/src/main/java/UI/ShellSelection.java
+++
@@ -1,8 +0,0 @@
-package UI;
-
-/**
- * Created by Andrei_Bugakov on 11/5/2015.
- */
-public class ShellSelection {
-
-}
--- src/main/java/UI/ShellSelection.java
+++
@@ -1,8 +0,0 @@
-package main.java.UI;
-
-/**
- * Created by Andrei_Bugakov on 11/5/2015.
- */
-public class ShellSelection {
-
-}
