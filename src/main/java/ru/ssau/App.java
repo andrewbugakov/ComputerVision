696ee0323e7da8f69711a5f3d719b020254ff924
Sergei Ermakov <sbutterfly@outlook.com>
2015-11-24 22:18:18 +0400

    Added logging

--- src/main/java/ru/ssau/App.java
+++ src/main/java/ru/ssau/App.java
@@ -1,5 +1,7 @@
 package ru.ssau;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -7,11 +9,14 @@ import ru.ssau.I18n.Strings;
 
 public class App 
 {
+	private static Logger logger = LogManager.getLogger(App.class.getName());
+
 	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
 	public static void main(String[] args) throws Exception {
+		logger.debug("Application has started");
+
 		String filePath = "src/main/resources/images/marble.jpg";
-		
 		
 		Mat newImage = Imgcodecs.imread(filePath);
 
