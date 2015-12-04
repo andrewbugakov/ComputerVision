63022a46bf25044501343f72fa155755a20f2ce2
Sergey Ermakov <sbutterfly@outlook.com>
2015-12-05 00:28:25 +0400

    Implemented base WPF OpenCV project

---
+++ VideoFeatureMatching/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using VideoFeatureMatching.Utils;
+
+namespace VideoFeatureMatching
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Capture _capture = null;
+        private bool _captureInProgress;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                _capture = new Capture();
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            Mat frame = new Mat();
+            _capture.Retrieve(frame, 0);
+            Mat grayFrame = new Mat();
+            CvInvoke.CvtColor(frame, grayFrame, ColorConversion.Bgr2Gray);
+            Mat smallGrayFrame = new Mat();
+            CvInvoke.PyrDown(grayFrame, smallGrayFrame);
+            Mat smoothedGrayFrame = new Mat();
+            CvInvoke.PyrUp(smallGrayFrame, smoothedGrayFrame);
+
+            //Image<Gray, Byte> smallGrayFrame = grayFrame.PyrDown();
+            //Image<Gray, Byte> smoothedGrayFrame = smallGrayFrame.PyrUp();
+            Mat cannyFrame = new Mat();
+            CvInvoke.Canny(smoothedGrayFrame, cannyFrame, 100, 60);
+
+            //Image<Gray, Byte> cannyFrame = smoothedGrayFrame.Canny(100, 60);
+
+            Dispatcher.Invoke(() => {
+                captureImageBox.Source = frame.ToBitmapSource();
+                grayscaleImageBox.Source = grayFrame.ToBitmapSource();
+                smoothedGrayscaleImageBox.Source = smoothedGrayFrame.ToBitmapSource();
+                cannyImageBox.Source = cannyFrame.ToBitmapSource();
+            });
+        }
+
+        private void captureButtonClick(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    captureButton.Content = "Start Capture";
+                    _capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    captureButton.Content = "Stop";
+                    _capture.Start();
+                }
+
+                _captureInProgress = !_captureInProgress;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            if (_capture != null)
+                _capture.Dispose();
+        }
+
+        private void FlipHorizontalButtonClick(object sender, EventArgs e)
+        {
+            if (_capture != null) _capture.FlipHorizontal = !_capture.FlipHorizontal;
+        }
+
+        private void FlipVerticalButtonClick(object sender, EventArgs e)
+        {
+            if (_capture != null) _capture.FlipVertical = !_capture.FlipVertical;
+        }
+    }
+}
